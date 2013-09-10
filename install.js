244701c9a71a59e6675658ddc5819c47279b0295
sil <sil@kryogenix.org>
2013-09-10 11:31:49 +0100

    Change the hardcoded version of CasperJS that grunt-casperjs downloads from 1.0.3 to master. This commit is not for merging back into mainline because it does not solve the problem, it just kicks it down the road. Really, grunt-casperjs ought to be picking up the version of CasperJS that it requires from some sort of config, and defaulting to a known-good version. However, that does not seem to be easily possible with an install script, and so doing this would involve a more substantial rewrite of the way the package works.

--- install.js
+++ install.js
@@ -13,9 +13,8 @@ fs.existsSync = fs.existsSync || path.existsSync
 
 var libPath = path.join(__dirname, 'lib', 'casperjs')
 var tmpPath = path.join(__dirname, 'tmp')
-var downloadUrl = 'https://github.com/n1k0/casperjs/archive/1.0.3.zip'
-
-
+var casperVersion = "master"
+var downloadUrl = 'https://github.com/n1k0/casperjs/archive/' + casperVersion + '.zip'
 
 function isCasperInstalled(notInstalledCallback) {
     cp.exec("which casperjs", function(error, stdout, stderr) {
@@ -38,7 +37,7 @@ function unzipTheZippedFile() {
     zip.extractAllTo(libPath, true);
 
     if (process.platform != 'win32') {
-        var pathToCommand = path.join(libPath, 'casperjs-1.0.3', 'bin', 'casperjs');
+        var pathToCommand = path.join(libPath, 'casperjs-' + casperVersion, 'bin', 'casperjs');
         fs.symlinkSync(pathToCommand, './casperjs');
         var stat = fs.statSync(pathToCommand);
         if (!(stat.mode & 64)) {
